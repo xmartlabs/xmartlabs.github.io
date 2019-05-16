4dc4ffec98b75bead4d48e406b0bfe506a720a0f
lsarni <lucia@xmartlabs.com>
2019-05-16 11:50:03 -0300

    Go to button and README update

--- js/main.js
+++ js/main.js
@@ -8,6 +8,27 @@ function turnTargetBlankForExternalLinks() {
   }
 }
 
+function showGoToTop() {
+  $(window).scroll(function(){
+    if ($(this).width() <= 620 && $(this).scrollTop() > 100) {
+      if($('#goToTop').is(":hidden")) {
+        $('#goToTop').css({display: "flex"}).hide().fadeIn();
+      }
+    } else {
+      $('#goToTop').fadeOut();
+    }
+  });
+}
+
 $(document).ready(function() {
   turnTargetBlankForExternalLinks();
+
+  if($('#goToTop').length > 0){
+    showGoToTop();
+  }
+
+  $('#goToTop').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
 });
