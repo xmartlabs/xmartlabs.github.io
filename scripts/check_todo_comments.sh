6f03f05f13b198a167786612c5e4e8227adbce51
Matías Irland <matiasirland@gmail.com>
2020-05-13 18:36:36 -0300

    Split buld_check job in two different jobs

---
+++ scripts/check_todo_comments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+stop_ship_files=$(grep -rli '<!--.*TODO.*-->\|<!--.*STOPSHIP.*-->\|<!--.*FIXME.*-->' _posts/*.md)
+if [[ "$stop_ship_files" ]]; then
+	echo "Forbidden comment found." 
+	echo " Files: $stop_ship_files" 
+    exit 2
+fi
