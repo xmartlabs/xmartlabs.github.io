8ee6491592dbe5be42f1760c0618767a5d74e5ed
Martin Barreto <martin@xmartlabs.com>
2020-11-11 19:33:38 -0300

    Design/new site (#136)
    * add resources
    
    * blog redesign - new site
    
    * minor fix
    
    * added new js
    
    * animate menu on mobile devices, make nav and footer match exactly the web
    
    * change layout

--- js/main.js
+++ js/main.js
@@ -31,4 +31,27 @@ $(document).ready(function() {
     $("html, body").animate({ scrollTop: 0 }, 600);
     return false;
   });
+
+  $('.nav_dropdown').click(function(event) {
+    var nav_icon = $('.nav_icon');
+    if (nav_icon.css('display') != 'none') { // Current display
+        nav_icon.css('display', 'none');
+    } else { // Hide reply form
+        nav_icon.css('display', 'block');
+    }
+
+    var nav_svg = $('.nav_svg');
+    if (nav_svg.css('display') != 'none') { // Current display
+        nav_svg.css('display', 'none');
+    } else { // Hide reply form
+        nav_svg.css('display', 'flex');
+    }
+
+    var dropdown_list = $('.dropdown_list');
+    if (dropdown_list.css('display') == 'flex') { // Current display
+        dropdown_list.css('display', 'none');
+    } else { // Hide reply form
+        dropdown_list.css('display', 'flex');
+    }
+  });
 });
